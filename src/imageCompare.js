ff80a89da4c8d09320d4c0f54e7f276a44e98e1d
LarryG <larryg@klassitech.co.uk>
2024-10-15 14:46:51 +0100

    feat(refactored): updating code

--- src/imageCompare.js
+++ src/imageCompare.js
@@ -15,204 +15,204 @@ const browserName = settings.remoteConfig || BROWSER_NAME
 let fileName;
 let diffFile;
 
-module.exports = {
-  /**
-   * Take an image of the current page and saves it as the given filename.
-   * @method saveScreenshot
-   * @param {string} filename The complete path to the file name where the image should be saved.
-   * @param elementsToHide
-   * @param filename
-   * @param elementSnapshot
-   * @returns {Promise<void>}
-   */
-  takePageImage: async (filename, elementSnapshot, elementsToHide) => {
-    const resultDir = `./artifacts/visual-regression/original/${browserName}/${envName}/`;
-    const resultDirPositive = `${resultDir}positive/`;
+/**
+ * Take an image of the current page and saves it as the given filename.
+ * @method saveScreenshot
+ * @param {string} filename The complete path to the file name where the image should be saved.
+ * @param elementsToHide
+ * @param filename
+ * @param elementSnapshot
+ * @returns {Promise<void>}
+ */
+async function takePageImage(filename, elementSnapshot, elementsToHide){
+  const resultDir = `./artifacts/visual-regression/original/${browserName}/${envName}/`;
+  const resultDirPositive = `${resultDir}positive/`;
 
-    if (elementsToHide) {
-      await module.exports.hideElements(elementsToHide);
-    }
+  if (elementsToHide) {
+    await module.exports.hideElements(elementsToHide);
+  }
 
-    fs.ensureDirSync(resultDirPositive); // Make sure destination folder exists, if not, create it
-    const resultPathPositive = `${resultDirPositive}${filename}`;
+  fs.ensureDirSync(resultDirPositive); // Make sure destination folder exists, if not, create it
+  const resultPathPositive = `${resultDirPositive}${filename}`;
 
-    /** Logic to take an image of a whole page or an element image on a page */
-    if (elementSnapshot) {
-      let elem = await browser.$(elementSnapshot);
-      await elem.saveScreenshot(resultPathPositive, async (err) => {
-        await module.exports.timeoutErrormsg(err);
-      });
-    } else {
-      await browser.saveScreenshot(resultPathPositive, async (err) => {
-        await module.exports.timeoutErrormsg(err);
-      });
-    }
-
-    if (elementsToHide) {
-      await module.exports.showElements(elementsToHide);
-    }
-    console.log(`\t images saved to: ${resultPathPositive}`);
-  },
-
-  timeoutErrormsg: async (err) => {
-    await browser.pause(DELAY_500ms);
-    if (err) {
-      console.error(err.message);
-    }
-  },
-
-  /**
-   * Runs assertions and comparison checks on the taken images
-   * @param filename
-   * @param expected
-   * @param result
-   * @param value
-   * @returns {Promise<void>}
-   */
-  assertion(filename, expected, result, value) {
-    const baselineDir = `./visual-regression-baseline/${browserName}/${envName}/`;
-    const resultDir = `./artifacts/visual-regression/original/${browserName}/${envName}/`;
-    const resultDirPositive = `${resultDir}positive/`;
-    const resultDirNegative = `${resultDir}negative/`;
-
-    const diffDir = `./artifacts/visual-regression/diffs/${browserName}/${envName}/`;
-    const diffDirPositive = `${diffDir}positive/`;
-    const diffDirNegative = `${diffDir}negative/`;
-
-    fileName = filename;
-    const baselinePath = `${baselineDir}${filename}`;
-    const resultPathPositive = `${resultDirPositive}${filename}`;
-    fs.ensureDirSync(baselineDir); // Make sure destination folder exists, if not, create it
-    fs.ensureDirSync(diffDirPositive); // Make sure destination folder exists, if not, create it
-    this.expected = 0.2 || expected; // misMatchPercentage tolerance default 0.3%
-    if (!fs.existsSync(baselinePath)) {
-      // create new baseline image if none exists
-      console.log('\t WARNING: Baseline image does NOT exist.');
-      console.log(`\t Creating Baseline image from Result: ${baselinePath}`);
-      fs.writeFileSync(baselinePath, fs.readFileSync(resultPathPositive));
-    }
-    resemble.outputSettings({
-      errorColor: {
-        red: 225,
-        green: 0,
-        blue: 225,
-      },
-      errorType: 'movement',
-      transparency: 0.1,
-      largeImageThreshold: 1200,
+  /** Logic to take an image of a whole page or an element image on a page */
+  if (elementSnapshot) {
+    let elem = await browser.$(elementSnapshot);
+    await elem.saveScreenshot(resultPathPositive, async (err) => {
+      await module.exports.timeoutErrormsg(err);
     });
-    resemble(baselinePath)
-      .compareTo(resultPathPositive)
-      .ignoreAntialiasing()
-      .ignoreColors()
-      .onComplete(async (res) => {
-        result = await res;
+  } else {
+    await browser.saveScreenshot(resultPathPositive, async (err) => {
+      await module.exports.timeoutErrormsg(err);
+    });
+  }
+
+  if (elementsToHide) {
+    await module.exports.showElements(elementsToHide);
+  }
+  console.log(`\t images saved to: ${resultPathPositive}`);
+}
+
+timeoutErrormsg: async (err) => {
+  await browser.pause(DELAY_500ms);
+  if (err) {
+    console.error(err.message);
+  }
+}
+
+/**
+ * Runs assertions and comparison checks on the taken images
+ * @param filename
+ * @param expected
+ * @param result
+ * @param value
+ * @returns {Promise<void>}
+ */
+async function imageAssertion(filename, expected, result, value) {
+  const baselineDir = `./visual-regression-baseline/${browserName}/${envName}/`;
+  const resultDir = `./artifacts/visual-regression/original/${browserName}/${envName}/`;
+  const resultDirPositive = `${resultDir}positive/`;
+  const resultDirNegative = `${resultDir}negative/`;
+
+  const diffDir = `./artifacts/visual-regression/diffs/${browserName}/${envName}/`;
+  const diffDirPositive = `${diffDir}positive/`;
+  const diffDirNegative = `${diffDir}negative/`;
+
+  fileName = filename;
+  const baselinePath = `${baselineDir}${filename}`;
+  const resultPathPositive = `${resultDirPositive}${filename}`;
+  fs.ensureDirSync(baselineDir); // Make sure destination folder exists, if not, create it
+  fs.ensureDirSync(diffDirPositive); // Make sure destination folder exists, if not, create it
+  this.expected = 0.2 || expected; // misMatchPercentage tolerance default 0.3%
+  if (!fs.existsSync(baselinePath)) {
+    // create new baseline image if none exists
+    console.log('\t WARNING: Baseline image does NOT exist.');
+    console.log(`\t Creating Baseline image from Result: ${baselinePath}`);
+    fs.writeFileSync(baselinePath, fs.readFileSync(resultPathPositive));
+  }
+  resemble.outputSettings({
+    errorColor: {
+      red: 225,
+      green: 0,
+      blue: 225,
+    },
+    errorType: 'movement',
+    transparency: 0.1,
+    largeImageThreshold: 1200,
+  });
+  resemble(baselinePath)
+    .compareTo(resultPathPositive)
+    .ignoreAntialiasing()
+    .ignoreColors()
+    .onComplete(async (res) => {
+      result = await res;
+    });
+  /**
+   * @returns {Promise<void>}
+   */
+  this.value = async function () {
+    filename = await fileName;
+    const resultPathNegative = `${resultDirNegative}${filename}`;
+    const resultPathPositive = `${resultDirPositive}${filename}`;
+    while (typeof result === 'undefined') {
+      await browser.pause(DELAY_100ms);
+    }
+    const error = parseFloat(result.misMatchPercentage); // value this.pass is called with
+    fs.ensureDirSync(diffDirNegative); // Make sure destination folder exists, if not, create it
+
+    if (error > this.expected) {
+      diffFile = `${diffDirNegative}${filename}`;
+
+      const writeStream = fs.createWriteStream(diffFile);
+      await result.getDiffImage().pack().pipe(writeStream);
+      writeStream.on('error', (err) => {
+        console.log('this is the writeStream error ', err);
       });
-    /**
-     * @returns {Promise<void>}
-     */
-    this.value = async function () {
-      filename = await fileName;
-      const resultPathNegative = `${resultDirNegative}${filename}`;
-      const resultPathPositive = `${resultDirPositive}${filename}`;
-      while (typeof result === 'undefined') {
-        await browser.pause(DELAY_100ms);
-      }
-      const error = parseFloat(result.misMatchPercentage); // value this.pass is called with
-      fs.ensureDirSync(diffDirNegative); // Make sure destination folder exists, if not, create it
+      fs.ensureDirSync(resultDirNegative); // Make sure destination folder exists, if not, create it
+      fs.removeSync(resultPathNegative);
+      fs.moveSync(resultPathPositive, resultPathNegative, false);
+      console.log(`\t Create diff image [negative]: ${diffFile}`);
+    } else {
+      diffFile = `${diffDirPositive}${filename}`;
 
-      if (error > this.expected) {
-        diffFile = `${diffDirNegative}${filename}`;
-
-        const writeStream = fs.createWriteStream(diffFile);
-        await result.getDiffImage().pack().pipe(writeStream);
-        writeStream.on('error', (err) => {
-          console.log('this is the writeStream error ', err);
-        });
-        fs.ensureDirSync(resultDirNegative); // Make sure destination folder exists, if not, create it
-        fs.removeSync(resultPathNegative);
-        fs.moveSync(resultPathPositive, resultPathNegative, false);
-        console.log(`\t Create diff image [negative]: ${diffFile}`);
-      } else {
-        diffFile = `${diffDirPositive}${filename}`;
-
-        const writeStream = fs.createWriteStream(diffFile);
-        result.getDiffImage().pack().pipe(writeStream);
-        writeStream.on('error', (err) => {
-          console.log('this is the writeStream error ', err);
-        });
-      }
-    };
-    /**
-     * @returns {Promise<boolean>}
-     */
-    this.pass = async function () {
-      value = parseFloat(result.misMatchPercentage);
-      this.message = `image Match Failed for ${filename} with a tolerance difference of ${`${
-        value - this.expected
-      } - expected: ${this.expected} but got: ${value}`}`;
-      const baselinePath = `${baselineDir}${filename}`;
-      const resultPathNegative = `${resultDirNegative}${filename}`;
-      const pass = value <= this.expected;
-      const err = value > this.expected;
-
-      if (pass) {
-        console.log(`image Match for ${filename} with ${value}% difference.`);
-        await browser.pause(DELAY_1s);
-      }
-
-      if (err === true && program.opts().updateBaselineImage) {
-        console.log(
-          `${this.message}   images at:\n` +
-            `   Baseline: ${baselinePath}\n` +
-            `   Result: ${resultPathNegative}\n` +
-            `    cp ${resultPathNegative} ${baselinePath}`
-        );
-        await fs.copy(resultPathNegative, baselinePath, (err) => {
-          console.log(` All Baseline images have now been updated from: ${resultPathNegative}`);
-          if (err) {
-            console.error('The Baseline images were NOT updated: ', err.message);
-            throw err;
-          }
-        });
-      } else if (err) {
-        console.log(
-          `${this.message}   images at:\n` +
-            `   Baseline: ${baselinePath}\n` +
-            `   Result: ${resultPathNegative}\n` +
-            `   Diff: ${diffFile}\n` +
-            `   Open ${diffFile} to see how the image has changed.\n` +
-            '   If the Resulting image is correct you can use it to update the Baseline image and re-run your test:\n' +
-            `    cp ${resultPathNegative} ${baselinePath}`
-        );
-        throw `${err} - ${this.message}`;
-      }
-    };
-  },
-
-  /**
-   * hideElemements hide elements
-   * @param selectors
-   */
-  hideElements: async (selectors) => {
-    // if arg is no array make it one
-    selectors = typeof selectors === 'string' ? [selectors] : selectors;
-    for (let i = 0; i < selectors.length; i++) {
-      const script = `document.querySelectorAll('${selectors[i]}').forEach(element => element.style.opacity = '0')`;
-      await browser.execute(script);
+      const writeStream = fs.createWriteStream(diffFile);
+      result.getDiffImage().pack().pipe(writeStream);
+      writeStream.on('error', (err) => {
+        console.log('this is the writeStream error ', err);
+      });
     }
-  },
-
+  };
   /**
-   * showElemements show elements
-   * @param selectors
+   * @returns {Promise<boolean>}
    */
-  showElements: async (selectors) => {
-    // if arg is no array make it one
-    selectors = typeof selectors === 'string' ? [selectors] : selectors;
-    for (let i = 0; i < selectors.length; i++) {
-      const script = `document.querySelectorAll('${selectors[i]}').forEach(element => element.style.opacity = '1')`;
-      await browser.execute(script);
+  this.pass = async function () {
+    value = parseFloat(result.misMatchPercentage);
+    this.message = `image Match Failed for ${filename} with a tolerance difference of ${`${
+      value - this.expected
+    } - expected: ${this.expected} but got: ${value}`}`;
+    const baselinePath = `${baselineDir}${filename}`;
+    const resultPathNegative = `${resultDirNegative}${filename}`;
+    const pass = value <= this.expected;
+    const err = value > this.expected;
+
+    if (pass) {
+      console.log(`image Match for ${filename} with ${value}% difference.`);
+      await browser.pause(DELAY_1s);
     }
-  },
-};
+
+    if (err === true && program.opts().updateBaselineImage) {
+      console.log(
+        `${this.message}   images at:\n` +
+        `   Baseline: ${baselinePath}\n` +
+        `   Result: ${resultPathNegative}\n` +
+        `    cp ${resultPathNegative} ${baselinePath}`
+      );
+      await fs.copy(resultPathNegative, baselinePath, (err) => {
+        console.log(` All Baseline images have now been updated from: ${resultPathNegative}`);
+        if (err) {
+          console.error('The Baseline images were NOT updated: ', err.message);
+          throw err;
+        }
+      });
+    } else if (err) {
+      console.log(
+        `${this.message}   images at:\n` +
+        `   Baseline: ${baselinePath}\n` +
+        `   Result: ${resultPathNegative}\n` +
+        `   Diff: ${diffFile}\n` +
+        `   Open ${diffFile} to see how the image has changed.\n` +
+        '   If the Resulting image is correct you can use it to update the Baseline image and re-run your test:\n' +
+        `    cp ${resultPathNegative} ${baselinePath}`
+      );
+      throw `${err} - ${this.message}`;
+    }
+  };
+}
+
+/**
+ * hideElemements hide elements
+ * @param selectors
+ */
+async function hideElements(selectors) {
+  // if arg is no array make it one
+  selectors = typeof selectors === 'string' ? [selectors] : selectors;
+  for (let i = 0; i < selectors.length; i++) {
+    const script = `document.querySelectorAll('${selectors[i]}').forEach(element => element.style.opacity = '0')`;
+    await browser.execute(script);
+  }
+}
+
+/**
+ * showElemements show elements
+ * @param selectors
+ */
+async function showElements(selectors){
+  // if arg is no array make it one
+  selectors = typeof selectors === 'string' ? [selectors] : selectors;
+  for (let i = 0; i < selectors.length; i++) {
+    const script = `document.querySelectorAll('${selectors[i]}').forEach(element => element.style.opacity = '1')`;
+    await browser.execute(script);
+  }
+}
+
+module.exports = { takePageImage, imageAssertion, hideElements, showElements };
--- utils/common.js
+++ utils/common.js
@@ -1,28 +1,28 @@
-const verify = require('../src/imageCompare');
+const { takePageImage, imageAssertion, hideElements, showElements } = require('../src/imageCompare');
 
-module.exports = {
-    /**
-     * Visual comparison function
-     * @param fileName
-     * @returns {Promise<void>}
-     */
-    compareImage: async (fileName) => {
-        await verify.assertion(fileName);
-        await verify.value();
-        await verify.pass();
-    },
 
-    /**
-     * This take an image of a page or an element on a page
-     * fileName only = a whole page image
-     * fileName + elementSnapshot = take an image of an element on the page
-     * @param fileName {string}
-     * @param elementSnapshot {any}
-     * @param elementsToHide {string}
-     * @returns {Promise<void>}
-     */
-    takeImage: async (fileName, elementSnapshot, elementsToHide = '') => {
-        await verify.takePageImage(fileName, elementSnapshot, elementsToHide);
-        await browser.pause(DELAY_500ms);
-    },
+// module.exports = {
+/**
+ * Visual comparison function
+ * @param fileName
+ * @returns {Promise<void>}
+ */
+async function compareImage(fileName){
+    await imageAssertion(fileName);
 }
+
+/**
+ * This take an image of a page or an element on a page
+ * fileName only = a whole page image
+ * fileName + elementSnapshot = take an image of an element on the page
+ * @param fileName {string}
+ * @param elementSnapshot {any}
+ * @param elementsToHide {string}
+ * @returns {Promise<void>}
+ */
+async function takeImage(fileName, elementSnapshot, elementsToHide = ''){
+    await takePageImage(fileName, elementSnapshot, elementsToHide);
+    await browser.pause(DELAY_500ms);
+}
+// }
+module.exports = { compareImage, takeImage };
